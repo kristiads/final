3a51afadb8b20c622392606eb97819f3ca0b4eb9
Kristia Sianipar <kristia.sianipar@kellogg.northwestern.edu>
2018-03-12 22:12:25 -0500

    Final submission

--- App.js
+++ App.js
@@ -1,23 +1,204 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ListView, StyleSheet, View } from 'react-native';
+import { Body, Title, Right, Container, Header, Content, Button, Icon, List, ListItem, Text } from 'native-base';
 
 export default class App extends React.Component {
-  render() {
+  constructor() {
+    super();
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      todo: []
+    }
+  }
+
+  // Retrieve the list of todo from Airtable
+  getToDoTask() {
+    // Airtable API endpoint, replace with your own
+    let airtableUrl = "https://api.airtable.com/v0/appp3WRiTUsSSJIxh/todo?&view=Grid%20view";
+
+    // Needed for Airtable authorization, replace with your own API key
+    let requestOptions = {
+      headers: new Headers({
+        'Authorization': 'Bearer keyzaXFuju0bSrl1g'
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.setState({
+        todo: json.records
+      });
+    });
+  }
+
+  // Runs when the application loads (i.e. the "App" component "mounts")
+  componentDidMount() {
+    this.getToDoTask(); // refresh the list when we're done
+  }
+
+  // Upvote an idea
+  increasePriority(data, secId, rowId, rowMap) {
+    // Slide the row back into place
+    rowMap[`${secId}${rowId}`].props.closeRow();
+
+    // Airtable API endpoint
+    let airtableUrl = "https://api.airtable.com/v0/appp3WRiTUsSSJIxh/todo/" + data.id;
+
+    // Needed for Airtable authorization
+    let requestOptions = {
+      method: 'PATCH',
+      headers: new Headers({
+        'Authorization': 'Bearer keyzaXFuju0bSrl1g', // replace with your own API key
+        'Content-type': 'application/json'
+      }),
+      body: JSON.stringify({
+        fields: {
+          priority: data.fields.priority + 1
+        }
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.getToDoTask(); // refresh the list when we're done
+    });
+  }
+
+  // Downvote an idea
+  decreasePriority(data, secId, rowId, rowMap) {
+    // Slide the row back into place
+    rowMap[`${secId}${rowId}`].props.closeRow();
+
+    // Airtable API endpoint
+    let airtableUrl = "https://api.airtable.com/v0/appp3WRiTUsSSJIxh/todo/" + data.id;
+
+    // Needed for Airtable authorization
+    let requestOptions = {
+      method: 'PATCH',
+      headers: new Headers({
+        'Authorization': 'Bearer keyzaXFuju0bSrl1g', // replace with your own API key
+        'Content-type': 'application/json'
+      }),
+      body: JSON.stringify({
+        fields: {
+          priority: data.fields.priority - 1
+        }
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.getToDoTask(); // refresh the list when we're done
+    });
+  }
+
+  // Ignore an idea
+  ignoreToDoTask(data, secId, rowId, rowMap) {
+    // Slide the row back into place
+    rowMap[`${secId}${rowId}`].props.closeRow();
+
+    // Create a new array that has the idea removed
+    let newIdeasData = this.state.todo.slice();
+    newIdeasData.splice(rowId, 1);
+
+    // Set state
+    this.setState({
+      todo: newIdeasData
+    });
+  }
+
+  // Delete an idea
+  deleteToDoTask(data, secId, rowId, rowMap) {
+    // Slide the row back into place
+    rowMap[`${secId}${rowId}`].props.closeRow();
+
+    // Create a new array that has the idea removed
+    let newIdeasData = this.state.todo.slice();
+    newIdeasData.splice(rowId, 1);
+
+    // Airtable API endpoint
+    let airtableUrl = "https://api.airtable.com/v0/appp3WRiTUsSSJIxh/todo/" + data.id;
+
+    // Needed for Airtable authorization
+    let requestOptions = {
+      method: 'DELETE',
+      headers: new Headers({
+        'Authorization': 'Bearer keyzaXFuju0bSrl1g', // replace with your own API key
+        'Content-type': 'application/json'
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.getToDoTask(); // refresh the list when we're done
+    });
+  }
+
+  // The UI for each row of data
+  renderRow(data) {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ListItem style={{ paddingLeft: 20, paddingRight: 20 }}>
+        <Body>
+          <Text>{data.fields.task}</Text>
+        </Body>
+        <Right>
+          <Text note>{data.fields.priority}</Text>
+        </Right>
+      </ListItem>
+    )
+  }
+
+  // The UI for what appears when you swipe right
+  renderSwipeRight(data, secId, rowId, rowMap) {
+    return (
+      <Button full success onPress={() => this.increasePriority(data, secId, rowId, rowMap)}>
+        <Icon active name="arrow-up" />
+      </Button>
+    )
+  }
+
+  // The UI for what appears when you swipe left
+  renderSwipeLeft(data, secId, rowId, rowMap) {
+    return (
+
+      <Button full danger onPress={() => this.ignoreToDoTask(data, secId, rowId, rowMap)}>
+        <Icon active name="trash" />
+      </Button>
+    )
+  }
+
+  render() {
+    let rows = this.ds.cloneWithRows(this.state.todo);
+    return (
+      <Container>
+        <Header>
+          <Body>
+            <Title>Life Manager</Title>
+          </Body>
+        </Header>
+        <Content>
+          <List
+            dataSource={rows}
+            renderRow={(data) => this.renderRow(data)}
+            renderLeftHiddenRow={(data, secId, rowId, rowMap) => this.renderSwipeRight(data, secId, rowId, rowMap)}
+            renderRightHiddenRow={(data, secId, rowId, rowMap) => this.renderSwipeLeft(data, secId, rowId, rowMap)}
+            leftOpenValue={75}
+            rightOpenValue={-75}
+          />
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
